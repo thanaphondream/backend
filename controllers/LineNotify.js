d2f17156137fea509abcdfb65c250ad1e2929c5d
DREAM <voigkgky@gmail.com>
2024-04-27 18:23:53 +0700

    first commit

--- controllers/LineNotify.js
+++ controllers/LineNotify.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
 
+//เปลี่ยน Token ด้วยน่ะครับ เช่น const token = 'waRRbLw3mIM7hEcyKaiUfNadR1O9zcMNloxAORZVTYx' ตรงไฟล์ payment.js บรรทัดที่ 4 ครับ
+
 exports.Linenotifys = async (token, message) => {
     try {
         const response = await axios.post('https://notify-api.line.me/api/notify', 
