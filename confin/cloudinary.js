706e0575c9a803cce0bcc7ea10fb8acecc301a29
DREAM <voigkgky@gmail.com>
2024-09-29 01:56:28 +0700

    save new

--- confin/cloudinary.js
+++ confin/cloudinary.js
@@ -1,12 +1,11 @@
 const cloudinary = require('cloudinary').v2;
 
-// Configure Cloudinary using environment variables
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,  // Use environment variable for cloud name
-    api_key: process.env.CLOUDINARY_API_KEY,        // Use environment variable for API key
-    api_secret: process.env.CLOUDINARY_API_SECRET,  // Use environment variable for API secret
-    secure: true                                    // Enable HTTPS
-});
 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true  
+});
 
 module.exports = cloudinary;
