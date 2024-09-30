022b1430fc5ebe3b3a60a2f1daaf281c5f1e4d74
DREAM <voigkgky@gmail.com>
2024-09-30 21:49:02 +0700

    sevver02

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,6 +1,6 @@
 const { default: axios } = require("axios")
 const db = require("../models/db")
-// const cloudUpload = require("../middlewares/cloudUpload");
+const cloudUpload = require("../middlewares/cloudUpload");
 
 exports.profilePost = async (req, res, next) => {
     try{
@@ -33,47 +33,47 @@ exports.userId = async (req, res, next) => {
     }
 }
 
-// exports.profileUpdate = async (req, res, next) => {
-//     try {
-//       const { id } = req.params;
-//       const file = req.file; 
+exports.profileUpdate = async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const file = req.file; 
   
-//       const imagePromise = req.files.map((file) => {
-//         return cloudUpload(file.path);
-//       });
-//       const imageUrlArray = await Promise.all(imagePromise);
-//       const imageUrl = imageUrlArray[0];
+      const imagePromise = req.files.map((file) => {
+        return cloudUpload(file.path);
+      });
+      const imageUrlArray = await Promise.all(imagePromise);
+      const imageUrl = imageUrlArray[0];
   
-//       const profile = await db.profile.update({
-//         where: { id: parseInt(id) },
-//         data: { image: imageUrl }
-//       });
+      const profile = await db.profile.update({
+        where: { id: parseInt(id) },
+        data: { image: imageUrl }
+      });
   
-//       res.json({ msg: 'Profile updated successfully', profile });
-//     } catch (err) {
-//       next(err);
-//     }
-// };
+      res.json({ msg: 'Profile updated successfully', profile });
+    } catch (err) {
+      next(err);
+    }
+};
 
-// exports.profileUpdateimabg = async (req, res, next) => {
-//     try {
-//         const { id } = req.params;
-//         const file = req.file; 
-//         // const { imagebg } = req.body
+exports.profileUpdateimabg = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const file = req.file; 
+        // const { imagebg } = req.body
     
-//         const imagePromise = req.files.map((file) => {
-//           return cloudUpload(file.path);
-//         });
-//         const imageUrlArray = await Promise.all(imagePromise);
-//         const imageUrl = imageUrlArray[0];
+        const imagePromise = req.files.map((file) => {
+          return cloudUpload(file.path);
+        });
+        const imageUrlArray = await Promise.all(imagePromise);
+        const imageUrl = imageUrlArray[0];
     
-//         const profile = await db.profile.update({
-//           where: { id: parseInt(id) },
-//           data: { imagebg: imageUrl }
-//         });
+        const profile = await db.profile.update({
+          where: { id: parseInt(id) },
+          data: { imagebg: imageUrl }
+        });
     
-//         res.json({ msg: 'Profile updated successfully', profile });
-//       } catch (err) {
-//         next(err);
-//       }
-// }
+        res.json({ msg: 'Profile updated successfully', profile });
+      } catch (err) {
+        next(err);
+      }
+}
